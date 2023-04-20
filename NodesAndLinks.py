873cb33c447c4ad5f9f0a1780a074e467f8e28a7
Elvira van der Ven <sitde@hr.nl>
2023-04-20 14:21:54 +0200

    Data wordt nu niet in een lijst maar in een array van 3x3 geplaatst

--- NodesAndLinks.py
+++ NodesAndLinks.py
@@ -1,18 +1,21 @@
+side = 3
+
 class Node:
-	def __init__(self):
-		self.links = []
+        def __init__(self):
+                self.links = []
 
 class Link:
-	def __init__(self, inNode, outNode):
-		self.weight = 1
-		self.inNode = inNode
+        def __init__(self, inNode, outNode):
+                self.weight = 1
+                self.inNode = inNode
 
-		outNode.links.append(self)
+                outNode.links.append(self)
 
-inNodes = [Node () for i in range(9)]
+inNodes = [[Node () for column in range(side)]for row in range(side)]
 outNodes = [Node () for i in range(2)]
 
 links = []
-for inNode in inNodes:
-	for outNode in outNodes:
-		links.append(Link(inNode, outNode))
+for inRow in inNodes:
+        for inNode in inRow:
+                for outNode in outNodes:
+                        links.append(Link(inNode, outNode))
