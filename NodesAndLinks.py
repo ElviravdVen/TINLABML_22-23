7ea15043b43df77c80b3436c918e28d829a4d546
Elvira van der Ven <sitde@hr.nl>
2023-04-20 14:24:20 +0200

    Node class heeft een getValue functie

--- NodesAndLinks.py
+++ NodesAndLinks.py
@@ -4,6 +4,12 @@ class Node:
         def __init__(self):
                 self.links = []
 
+        def getValue(self):
+                sum = 0
+                for link in self.links:
+                        sum +=link.getValue()
+                return sum
+
 class Link:
         def __init__(self, inNode, outNode):
                 self.weight = 1
@@ -11,11 +17,13 @@ class Link:
 
                 outNode.links.append(self)
 
-inNodes = [[Node () for column in range(side)]for row in range(side)]
+inNodes = [[Node () for column in range(side)]for row in 
+range(side)]
 outNodes = [Node () for i in range(2)]
 
 links = []
 for inRow in inNodes:
         for inNode in inRow:
                 for outNode in outNodes:
-                        links.append(Link(inNode, outNode))
+                        links.append(Link(inNode, 
+outNode))
