580fa2d758d434efce27c1585fbecd4a3de14aac
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2023-04-22 09:01:18 +0200

    Added zsh-autosuggestions for Ubuntu

---
+++ ansible/install_zsh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+sudo apt update
