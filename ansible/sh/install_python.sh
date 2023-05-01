0e74173df9714d41671eb5b2c936ddd9ac757994
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2023-05-02 01:45:32 +0200

    Improved add ppa

--- ansible/sh/install_python.sh
+++ ansible/sh/install_python.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+# https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
+
 echo "Install software-properties-common"
 sudo apt install software-properties-common 
 
 echo "Reinstall ca-certificates"
 sudo apt-get install --reinstall ca-certificates 
 
+# TODO
+# https://stackoverflow.com/questions/68992799/warning-apt-key-is-deprecated-manage-keyring-files-in-trusted-gpg-d-instead
+
+echo "Sign"
+sudo apt-key adv --keyserver "hkp://keyserver.ubuntu.com:80" --recv "F23C5A6CF475977595C89F51BA6932366A755776"
+
 echo "Add ppa"
-sudo add-apt-repository 'deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main'
+sudo add-apt-repository "deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu focal main "
+sudo add-apt-repository "deb-src https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu focal main "
 
 echo "Update"
 sudo apt update 
