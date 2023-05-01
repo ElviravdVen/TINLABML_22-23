25a325d72f082bb28337259fb126357481f531a2
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2023-05-01 21:55:28 +0200

    Improved python installation script

--- ansible/sh/install_python.sh
+++ ansible/sh/install_python.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade -y
+echo "Install software-properties-common"
+sudo apt install software-properties-common 
 
-echo "Update apt"
-echo | sudo apt install software-properties-common -y
+echo "Reinstall ca-certificates"
+sudo apt-get install --reinstall ca-certificates 
 
 echo "Add ppa"
-echo | sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository 'deb https://ppa.launchpadcontent.net/deadsnakes/ppa/ubuntu jammy main'
+
+echo "Update"
+sudo apt update 
 
 echo "Install python 3.10"
-sudo apt install python3.10
+sudo apt install python3.10 
 
 echo "Install pip"
-sudo apt install python3-pip -y
+sudo apt install python3-pip 
+
+echo "Make python 3.10 default"
+sudo ln -s /usr/bin/python3.10 /usr/bin/python
