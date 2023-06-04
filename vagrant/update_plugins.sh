5204b4ff865d43d5f02ae68e6069347861fe1d3a
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2023-06-04 15:01:27 +0200

    Added scp plugin

--- vagrant/update_plugins.sh
+++ vagrant/update_plugins.sh
@@ -2,4 +2,9 @@
 
 echo "Update Plugins"
 vagrant plugin update
+
+echo "Install Vagrant VBGuest"
 vagrant plugin install vagrant-vbguest
+
+echo "Install Vagrant SCP"
+vagrant plugin install vagrant-scp
